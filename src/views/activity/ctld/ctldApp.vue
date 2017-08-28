7d4a3fd4c11176aa53b78ad1301226ab239aeaa6
fackerzuo <562086977@qq.com>
2017-08-28 14:03:19 +0800

    创建活动页面

---
+++ src/views/activity/ctld/ctldApp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mainWrapper">
+        <ul class="container">
+            <li class="page"></li>
+        </ul>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="less">
+
+</style>
