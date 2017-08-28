8934e3713a2263152f7f6e3c0b78b2ab372f533a
fackerzuo <562086977@qq.com>
2017-08-28 14:33:11 +0800

    添加部分新标签的样式规范

--- src/views/activity/ctld/ctldApp.vue
+++ src/views/activity/ctld/ctldApp.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="mainWrapper">
         <ul class="container">
-            <li class="page"></li>
+            <li class="page">1</li>
         </ul>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+    import Lib from 'assets/js/Lib'
+    export default {
 
+    }
 </script>
 
 <style lang="less">
