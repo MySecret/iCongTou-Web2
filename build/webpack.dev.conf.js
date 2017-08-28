26370e0964a1f8f50fd5a2fe90b2ca2c1ed3e535
Perhaps <1330101002@qq.com>
2017-08-28 09:43:02 +0800

    api全局变量

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -19,7 +19,8 @@ module.exports = merge(baseWebpackConfig, {
   devtool: '#cheap-module-eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': config.dev.env
+      'process.env': config.dev.env,
+      PublicUrlAPI: JSON.stringify("http://qa-api.icongtou.com/api/rest/")
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
