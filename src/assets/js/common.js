50fb9dba232a5cc90ac32e0136b05f6bbd3a0716
fackerzuo <562086977@qq.com>
2017-09-01 18:49:42 +0800

    打包

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -166,7 +166,7 @@ var Rxports = {
 		return weekDay[date.Format('w')]
 	},
     WXshare: function(setUpInfo, callback) {
-		var shareUrl = location.href.split('#')[0];
+		var shareUrl = window.location.href;
 		var shareTitle_t = setUpInfo.shareTitle_t || setUpInfo.title;
 		var lineLink_t = shareUrl;
 		var imgUrl_t = setUpInfo.imgUrl_t || setUpInfo.imageUrl;
